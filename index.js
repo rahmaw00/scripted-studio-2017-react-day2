f822ab23dff0f527160fca7d8d892213c822dbbe
rahmaw00 <ramata.will@gmail.com>
2017-12-18 23:25:21 +0000

    react 1 and 2

--- index.js
+++ index.js
@@ -5,31 +5,36 @@
 //Part Two: Add a delete button to each list element.
 
 function List(props) {
-    //TODO: Create list from props.
     return (<ul>
+      {
+        props.items.map(function(item, index) {
+          return <li key={index}>{item}</li>;
+        })}
     </ul>);
 };
 
 var App = React.createClass({
   getInitialState: function() {
-    //TODO: Set the initial state.
     return {
-        
+      term: '',
+      items: []
     };
   },
   onChange: function(event) {
-    //TODO: Save current value in input field.
+    this.setState({ term: event.target.value });
   },
   onSubmit: function(event) {
-      //TODO: Add to list.
+    event.preventDefault();
+    this.setState({
+      items: [...this.state.items, this.state.term]
+    });
   },
   render: function() {
-    //TODO: Add event handlers to input and button.
     return (
       <div>
         <h2>Todo List</h2>
-        <form className="App">
-          <input/>
+        <form className="App" onSubmit={this.onSubmit}>
+          <input value={this.state.term} onChange={this.onChange}/>
           <button>Add</button>
         </form>
         <List items={this.state.items} />
