f46137d6100ee8aa9adc021b3cb75023299635ae
rahmaw00 <ramata.will@gmail.com>
2017-12-18 23:32:52 +0000

    new version

--- index.js
+++ index.js
@@ -47,3 +47,70 @@ ReactDOM.render(
   <App/>,
   document.getElementById('container')
 );
+
+
+var DeleteButton = React.createClass({
+  deleteItem: function() {
+    this.props.onDelete(this.props.index);
+  },
+  render: function() {
+    return (
+      <button onClick={this.deleteItem}>Delete</button>
+    );
+  }
+});
+
+function List(props) {
+  return (<ul>
+      {
+      props.items.map(function(item, index) {
+        return (<li id={index} key={index}>
+            {item} 
+            <DeleteButton index={index} onDelete={props.onDelete}/>
+        </li>);
+      })}
+  </ul>);
+};
+
+var App = React.createClass({
+  getInitialState: function() {
+    return {
+      term: '',
+      items: []
+    };
+  },
+  onChange: function(event) {
+    this.setState({ term: event.target.value });
+  },
+  onSubmit: function(event) {
+    event.preventDefault();
+    this.setState({
+      term: '',
+      items: [...this.state.items, this.state.term]
+    });
+  },
+  onDelete: function(index) {
+    this.state.items.splice(index, 1);
+    this.setState({
+      items: this.state.items
+    })
+  },
+  render: function() {
+    return (
+      <div>
+        <h2>Todo List</h2>
+        <form className="App" onSubmit={this.onSubmit}>
+          <input value={this.state.term} onChange={this.onChange} />
+          <button>Add</button>
+        </form>
+        <List items={this.state.items} onDelete={this.onDelete}/>
+      </div>
+    );
+  }
+});
+
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
+);
