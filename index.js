f6ced09ebe920822d6d1f04c4d4f93e16354b3fd
efuquen <efuquen@gmail.com>
2017-12-18 20:39:45 +0000

    add skeleton code

--- index.js
+++ index.js
@@ -3,3 +3,42 @@
 
 //Part One: Implement the TODO App from here: https://goo.gl/vySbPk
 //Part Two: Add a delete button to each list element.
+
+function List(props) {
+    //TODO: Create list from props.
+    return (<ul>
+    </ul>);
+};
+
+var App = React.createClass({
+  getInitialState: function() {
+    //TODO: Set the initial state.
+    return {
+        
+    };
+  },
+  onChange: function(event) {
+    //TODO: Save current value in input field.
+  },
+  onSubmit: function(event) {
+      //TODO: Add to list.
+  },
+  render: function() {
+    //TODO: Add event handlers to input and button.
+    return (
+      <div>
+        <h2>Todo List</h2>
+        <form className="App">
+          <input/>
+          <button>Add</button>
+        </form>
+        <List items={this.state.items} />
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('container')
+);
